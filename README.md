7025b461dda3fab49341537943dfd67150eea1d0
Navinoxx <lorenzo0100@hotmail.com>
2023-04-01 20:27:46 -0300

    Version 2.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Color Picker
+***
+Final Version
+
+## Technologies
+***
+A list of technologies used within the project:
+* [HTML5]
+* [Bootstrap](https://getbootstrap.com): Version 5.3
+* [JavaScript]
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const body = document.querySelector('body')
 const colorPicker = document.querySelector('#colorPicker')
 const colorHEX = document.querySelector('#colorHEX')
@@ -8,8 +10,22 @@ const hue = document.querySelector('#hue')
 const saturation = document.querySelector('#saturation')
 const lightness = document.querySelector('#lightness')
 
+const botones = document.querySelector('#botones')
+const saveColor = document.querySelector('#saveColor')
+const template = document.querySelector('#template').content
+const fragment = document.createDocumentFragment()
+
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
+
+let coloresGuardados = []
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  if(localStorage.getItem('colores')) {
+      coloresGuardados = JSON.parse(localStorage.getItem('colores'))
+  }
+  mostrarGuardados()
+})
 
 if (toastTrigger) {
   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
@@ -17,68 +33,115 @@ if (toastTrigger) {
     toastBootstrap.show()
     navigator.clipboard.writeText(colorPicker.value)
   })
+  saveColor.addEventListener('click', (e) => {
+    toastBootstrap.show()
+    navigator.clipboard.writeText(e.target.textContent)
+    
+  })
 }
 
 colorPicker.addEventListener('input', () => {
     setColor()
 })
 
+botones.addEventListener('click', (e) => {
+  boton(e)
+})
+
+const boton = e => {
+  if (e.target.classList.contains('btn-danger')) {
+    coloresGuardados = []
+    saveColor.innerHTML = ""
+    localStorage.clear()
+  }
+  if (coloresGuardados.includes(colorHEX.value)) {
+    return
+  }
+  if (e.target.classList.contains('btn-success')) {
+    coloresGuardados.push(colorHEX.value)
+    localStorage.setItem('colores', JSON.stringify(coloresGuardados)) 
+    mostrarGuardados()
+  }
+  e.stopPropagation()
+}
+
+const mostrarGuardados = () => {
+  saveColor.innerHTML = ""
+  coloresGuardados.forEach(color => {
+    const clone = template.cloneNode(true)
+    clone.querySelector('.card-text').textContent = color
+    clone.querySelector('.card-body').style.backgroundColor = color
+    fragment.appendChild(clone)
+  })  
+  saveColor.appendChild(fragment)
+}
+
 function setColor() {
-    body.style.backgroundColor = colorPicker.value
-    colorHEX.value = colorPicker.value
+  body.style.backgroundColor = colorPicker.value
+  colorHEX.value = colorPicker.value
 
-    const numHEX = JSON.stringify(colorPicker.value)
-    const sinHEX = numHEX.slice(2, 8)
-    const aRgbHex = sinHEX.match(/.{1,2}/g);
-    const aRgb = [
-        parseInt(aRgbHex[0], 16),
-        parseInt(aRgbHex[1], 16),
-        parseInt(aRgbHex[2], 16)
-        ];
-        
-    red.value = aRgb[0]
-    green.value = aRgb[1]
-    blue.value = aRgb[2]
+  const numHEX = JSON.stringify(colorPicker.value)
+  const sinHEX = numHEX.slice(2, 8)
+  const aRgbHex = sinHEX.match(/.{1,2}/g);
+  const aRgb = [
+      parseInt(aRgbHex[0], 16),
+      parseInt(aRgbHex[1], 16),
+      parseInt(aRgbHex[2], 16)
+      ];
+      
+  red.value = aRgb[0]
+  green.value = aRgb[1]
+  blue.value = aRgb[2]
+  
+  function rgbToHsl(r, g, b) {
+          r /= 255, g /= 255, b /= 255;
+      
+          let cmin = Math.min(r,g,b),
+          cmax = Math.max(r,g,b),
+          delta = cmax - cmin,
+          h = 0,
+          s = 0,
+          l = 0;
     
-    function rgbToHsl(r, g, b) {
-            r /= 255, g /= 255, b /= 255;
+      // Calculate hue
+      // No difference
+      if (delta == 0)
+        h = 0;
+      // Red is max
+      else if (cmax == r)
+        h = ((g - b) / delta) % 6;
+      // Green is max
+      else if (cmax == g)
+        h = (b - r) / delta + 2;
+      // Blue is max
+      else
+        h = (r - g) / delta + 4;
+    
+      h = Math.round(h * 60);
         
-            let max = Math.max(r, g, b), min = Math.min(r, g, b);
-            let h, s, l = (max + min) / 2;
+      // Make negative hues positive behind 360°
+      if (h < 0)
+          h += 360;
+    
+      // Calculate lightness
+      l = (cmax + cmin) / 2;
+    
+      // Calculate saturation
+      s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
         
-            if (max == min) {
-            h = 0; 
-            } else {
-            let d = max - min;
-            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-            switch (max) {
-                case r:
-                    h = ((g - b) / d) % 6;
-                    break;
-                    case g:
-                    h = (b - r) / d + 2;
-                    break;
-                    case b:
-                    h = (r - g) / d + 4;
-                    break;
-            }
-            h /= 1.69;
-            if (h < 0) h++;
-            }
-            
-            h = Math.round(h * 100);
-            s = Math.round(s * 100);
-            l = Math.round(l * 100);
+      // Multiply l and s by 10
+          s = Math.round(s * 100);
+          l = Math.round(l * 100);
 
-            return [h, s, l];
+          return [h, s, l];
 
-        }
-        
-        const [h, s, l] = rgbToHsl(aRgb[0],aRgb[1],aRgb[2])
+      }
+      
+      const [h, s, l] = rgbToHsl(aRgb[0],aRgb[1],aRgb[2])
 
-        hue.value = [h, s, l][0]
-        saturation.value = [h, s, l][1] + "%"
-        lightness.value = [h, s, l][2] + "%"
+      hue.value = [h, s, l][0]
+      saturation.value = [h, s, l][1] + "%"
+      lightness.value = [h, s, l][2] + "%"
 
 }
 
